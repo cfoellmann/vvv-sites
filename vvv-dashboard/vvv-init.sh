96d2834e706185d268c71dc524e111029b6babf8
Christian Foellmann <foellmann@foe-services.de>
2015-04-13 12:29:56 +0200

    proper vvv-dashboard provisioning

--- vvv-dashboard/vvv-init.sh
+++ vvv-dashboard/vvv-init.sh
@@ -1,33 +1,25 @@
 # Provision vvv-dashboard by @topdown
 echo -e "\n\n "
 echo -e "\n================================== "
-echo -e "\n Provision vvv-dashboard"
+echo -e "\n Provision topdown/VVV-Dashboard"
 echo -e "\n================================== "
 
 # Constants
-DIR="$(dirname $SITE_CONFIG_FILE)"
-DESTDIR="/srv/www/dashboard"
+DESTDIR="/srv/www/default"
 
 # Install pimpmylog via Composer
-if [[ ! -d $DESTDIR ]]; then
+if [[ ! -f /srv/www/dashboard-custom.php ]]; then
 	echo -e "\nInstalling vvv-dashboard..."
-	mkdir $DESTDIR
+	echo -e "\n\n "
 else
 	echo -e "\nUpdating vvv-dashboard..."
+	echo -e "\n\n "
 fi
 
-cd $DIR
-wget https://github.com/topdown/VVV-Dashboard/archive/master.tar.gz
-tar xzvf master.tar.gz
-
 # Copy over required files
-cp $DIR/VVV-Dashboard-master/index.php $DESTDIR/
-cp $DIR/VVV-Dashboard-master/dashboard-custom.php /srv/www/default/
-cp $DIR/VVV-Dashboard-master/style.css /srv/www/default/
-
-# Cleanup
-rm -rf $DIR/VVV-Dashboard-master
-rm -rf $DIR/master.tar.gz
+cd $DESTDIR
+wget https://raw.githubusercontent.com/topdown/VVV-Dashboard/master/index.php --output-document=dashboard-custom.php --progress=bar:force
+wget https://raw.githubusercontent.com/topdown/VVV-Dashboard/master/style.css --output-document=style.css --progress=bar:force
 
 echo -e "\n\n "
 echo -e "\n\033[33;32m...vvv-dashboard installed/updated.\033[0m"
